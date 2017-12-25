db2d035b96862eac70704a374d8f3e31bdd356c3
Vincenzo Nastro <vinctusor93@gmail.com>
2017-12-25 20:59:07 +0100

    eliminazione test di prova per fare POST request

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -26,15 +26,8 @@ export class SignupPage {
   registrazione(nome: HTMLInputElement, cognome: HTMLInputElement,username: HTMLInputElement, password: HTMLInputElement,tipologia: HTMLInputElement, data: HTMLInputElement, competenze: HTMLInputElement, titolo: HTMLInputElement, cf: HTMLInputElement, indirizzo: HTMLInputElement, citta: HTMLInputElement, provincia: HTMLInputElement , email: HTMLInputElement, tel: HTMLInputElement): void {
     var headers = new Headers();
     headers.append('Content-Type', 'application/x-www-form-urlencoded' );
-     let options = new RequestOptions({ headers: headers });
-      let postParams = {
-        username: 'username',
-        password: 'password'
-      }
-  //   let postParams = "username="+username.value+"&password="+password.value+"&nome="+nome.value+"&cognome="+cognome.value+"&tipologia="+tipologia.value+"&password="+password.value+"&password="+password.value+"&password="+password.value+"&password="+password.value+"&password="+password.value+"&password="+password.value+"&password="+password.value;
+   let options = new RequestOptions({ headers: headers });
    var myData = JSON.stringify({nome:nome.value ,cognome:cognome.value,username: username.value,password: password.value,tipologia:tipologia.value,nato:data.value,competenze:competenze.value,titolo:titolo.value,cf:cf.value,indirizzo:indirizzo.value,citta:citta.value,provincia:provincia.value,email:email.value,tel:tel.value});
-  // var myData = JSON.stringify({username: username.value,password: password.value});
- //console.log(myData)
   this.http.post('http://aiutiamoc.altervista.org/registrazioneUtente.php',myData,options).map(res => res.json()).subscribe(   data => {
   console.log(data);})
 }
