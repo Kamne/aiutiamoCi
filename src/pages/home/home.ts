2b5bb461cdff8fdb2f1540e4cb6b87eb59f90e74
Cri <m.ricciardi7@studenti.unisa.it>
2018-01-09 15:30:34 +0100

    Modificato grafica dell'home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import {RicercaPage} from '../ricerca/ricerca';
+import {RubricaPage} from '../rubrica/rubrica';
+import {BachecaPage} from '../bacheca/bacheca';
+import {EventPage} from '../event/event';
 /**
  * Generated class for the HomePage page.
  *
@@ -21,5 +25,16 @@ export class HomePage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
   }
-
+  cerca(){
+    this.navCtrl.push(RicercaPage);
+  }
+  bacheca(){
+    this.navCtrl.push(BachecaPage);
+  }
+  eventi(){
+    this.navCtrl.push(EventPage);
+  }
+  rubrica(){
+    this.navCtrl.push(RubricaPage);
+  }
 }
