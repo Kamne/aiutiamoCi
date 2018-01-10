45c69d4bebb25eda7e306323681bbd6a40d4694e
Vincenzo Nastro <vinctusor93@gmail.com>
2018-01-10 09:29:12 +0100

    event-crea page update

--- src/pages/event-crea/event-crea.ts
+++ src/pages/event-crea/event-crea.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Http, Headers, RequestOptions } from "@angular/http";
 
 /**
  * Generated class for the EventCreaPage page.
@@ -15,11 +16,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class EventCreaPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public http: Http) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EventCreaPage');
   }
 
+  creaEvento(img: HTMLInputElement, nome: HTMLInputElement, luogo: HTMLInputElement, date: HTMLInputElement, ora: HTMLInputElement, descrizione: HTMLInputElement, hashtag: HTMLInputElement): void {
+      console.log("image:"+img.value)
+
+  /*if(nome.value === "" || password.value === "")
+      console.log("valori errati")*/
+//  else{
+    var myData = JSON.stringify({nome: nome.value,password: luogo.value, img: img.value});
+  this.http.post('http://aiutiamoc.altervista.org/login.php',myData).map(res => res.json()).subscribe(   data => {
+  console.log(data);})
+//  }
+}
 }
