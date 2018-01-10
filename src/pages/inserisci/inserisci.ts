2110312ac9bc738e70e3e2af5bc46416ff5c5992
Vincenzo Nastro <vinctusor93@gmail.com>
2018-01-10 09:28:12 +0100

    update inserisci page

--- src/pages/inserisci/inserisci.ts
+++ src/pages/inserisci/inserisci.ts
@@ -3,6 +3,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
 import { SpeechRecognition } from '@ionic-native/speech-recognition';
 import { ChangeDetectorRef } from '@angular/core';
+import { Dialogs } from '@ionic-native/dialogs';
+import { Http, Headers, RequestOptions } from "@angular/http";
+import 'rxjs/add/operator/map';
+import { ShareService } from '../../providers/shareService';
 
 /**
  * Generated class for the InserisciPage page.
@@ -20,15 +24,50 @@ export class InserisciPage {
   matches: String[];
   isRecording = false;
   value:string[];
-  constructor(public navCtrl: NavController, public navParams: NavParams, private speechRecognition: SpeechRecognition, private cd: ChangeDetectorRef) {
+  richiesta:any
+  constructor(public navCtrl: NavController, public navParams: NavParams, private speechRecognition: SpeechRecognition, private cd: ChangeDetectorRef,public http: Http,public dialogs: Dialogs, public shareService : ShareService) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad InserisciPage');
   }
 
-  indietro(){
-    this.navCtrl.setRoot(HomePage);
+  indietro(testo: HTMLInputElement,patente: HTMLInputElement,automunito: HTMLInputElement,urgenza: HTMLInputElement){
+    console.log("prova"+Number(automunito.checked))
+    var headers = new Headers();
+
+
+  //  console.log(descr.value)
+    headers.append('Content-Type', 'application/x-www-form-urlencoded' );
+    let options = new RequestOptions({ headers: headers });
+    var myData = JSON.stringify({
+        automunito : Number(automunito.checked),
+        patente : Number(patente.checked),
+        urgenza : Number(urgenza.checked),
+        testo : testo.value,
+        tipologia : this.richiesta.value,
+        username : this.shareService.getUser().getUsername(),
+        tel : this.shareService.getUser().getTel(),
+        email : this.shareService.getUser().getEmail(),
+        data : "13/3/2015",
+        competenze : this.shareService.getUser().getToStringCompetenze()
+    });
+    console.log(myData)
+
+    this.http.post('http://aiutiamoc.altervista.org/Insert_Annuncio.php',myData,options).map(res => res.json()).subscribe(   data => {
+     console.log(data.success);
+     if(data.success){
+         console.log("nel if "+data.success);
+         this.navCtrl.setRoot(HomePage)
+     }
+
+     else{
+       this.dialogs.alert(data.message)
+     }
+         console.log(data.message)
+
+   })
+
   }
 
   getPermission() {
@@ -59,4 +98,8 @@ export class InserisciPage {
       console.info('could not set textarea-value');
     }
   }
+
+
+
+
 }
