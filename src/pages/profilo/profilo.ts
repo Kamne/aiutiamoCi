7cf262c66e06f90979746b29c9350e9d075d828b
Cri <m.ricciardi7@studenti.unisa.it>
2018-01-18 09:08:33 +0100

    Aggiornamento profilo

--- src/pages/profilo/profilo.ts
+++ src/pages/profilo/profilo.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import {AnnunciPage} from '../annunci/annunci';
+import { ShareService } from '../../providers/shareService';
+import { Utente } from '../../classes/utente';
+//import { Http, Headers, RequestOptions } from "@angular/http";
 /**
  * Generated class for the ProfiloPage page.
  *
@@ -15,11 +18,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProfiloPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              public shareService: ShareService) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfiloPage');
   }
+  /*
+  loadPage(p){
+    let myTemplate:any;
+    results: string[];
+  if(p=='annunci')
+    {
 
+      });
+   //   http.get(AnnunciPage).map((html:any) => this.myTemplate = html);
+    }
+  }*/
 }
