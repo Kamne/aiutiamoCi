52c750a6d3f9b45bcfc09402fed3dee584c7ef96
Vincenzo Nastro <vinctusor93@gmail.com>
2018-02-05 11:04:39 +0100

    modifiche signup

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -6,6 +6,9 @@ import { Dialogs } from '@ionic-native/dialogs';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Http, Headers, RequestOptions } from "@angular/http";
 import 'rxjs/add/operator/map';
+import { Events } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 /**
  * Generated class for the SignupPage page.
  *
@@ -21,8 +24,21 @@ import 'rxjs/add/operator/map';
 export class SignupPage {
 
   ciccio:FormGroup;
+  base64Image:String = "assets/imgs/my_logo.png"
+  citta:String
+  indirizzo:String
+  provincia:String
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, public dialogs: Dialogs, public formBuilder: FormBuilder) {
+  constructor(public events: Events,public camera: Camera,public navCtrl: NavController,public alertCtrl: AlertController, public navParams: NavParams, public http: Http, public dialogs: Dialogs, public formBuilder: FormBuilder) {
+    this.citta = ""
+    this.indirizzo = ""
+    this.provincia = ""
+    events.subscribe('maps', (maps) => {
+      this.indirizzo = maps[0];
+      this.citta = maps[1];
+      this.provincia = maps[2];
+  console.log('Welcome', maps);
+});
     this.ciccio = formBuilder.group({
        nome: [''],
        cognome: [''],
@@ -47,7 +63,7 @@ export class SignupPage {
     console.log('ionViewDidLoad SignupPage');
   }
   // registrazione(nome,cognome,username,password,tipologia,data,competenze,titolo,cf,indirizzo,citta,provincia,email,tel,iva)
-  registrazione(nome: HTMLInputElement, cognome: HTMLInputElement,username: HTMLInputElement, password: HTMLInputElement,tipologia: HTMLInputElement, data: HTMLInputElement, competenze: HTMLInputElement, titolo: HTMLInputElement, cf: HTMLInputElement, indirizzo: HTMLInputElement, citta: HTMLInputElement, provincia: HTMLInputElement , email: HTMLInputElement, tel: HTMLInputElement, iva: HTMLInputElement,descr: HTMLInputElement): void {
+  registrazione(nome: HTMLInputElement, cognome: HTMLInputElement,username: HTMLInputElement, password: HTMLInputElement,tipologia: HTMLInputElement, data: HTMLInputElement, competenze: HTMLInputElement, titolo: HTMLInputElement, cf: HTMLInputElement,email: HTMLInputElement, tel: HTMLInputElement, iva: HTMLInputElement,descr: HTMLInputElement): void {
     var headers = new Headers();
     console.log(descr.value)
     headers.append('Content-Type', 'application/x-www-form-urlencoded' );
@@ -62,15 +78,16 @@ export class SignupPage {
      competenze:competenze.value,
      titolo:titolo.value,
      cf:cf.value,
-     indirizzo:indirizzo.value,
-     citta:citta.value,
-     provincia:provincia.value,
+     indirizzo:this.indirizzo,
+     citta:this.citta,
+     provincia:this.provincia,
      email:email.value,
      tel:tel.value,
      iva:iva.value,
-     descr: descr.value
+     descr: descr.value,
+     img:this.base64Image
    });
-   console.log(myData)
+   console.log(myData);
  this.http.post('http://aiutiamoc.altervista.org/registrazioneUtente.php',myData,options).map(res => res.json()).subscribe(   data => {
   console.log(data.success);
   if(data.success){
@@ -87,7 +104,7 @@ export class SignupPage {
 }
 
 geolocalization(){
-  this.navCtrl.setRoot(MapPage)
+  this.navCtrl.push(MapPage)
 }
 onChange(v){
   if(v=="associazione"){
@@ -111,4 +128,80 @@ onChange(v){
 
   console.log(v);
 }
+
+chargeImage(){
+  let alert = this.alertCtrl.create({
+    title: 'Immagine del profilo',
+    cssClass:"alert",
+    inputs: [
+      {
+        name: 'foto',
+        type: 'radio',
+        value:'camera',
+        label:'Fotocamera'
+      },
+      {
+        name: 'foto',
+        type: 'radio',
+        value:'gallery',
+        label:'Galleria'
+      }
+    ],
+
+    buttons: ['Annulla' ,
+    {
+      text: 'Conferma',
+      handler: data => {
+          this.takeImg(data)
+      }
+    }]
+  });
+  alert.present();
+}
+takeImg(source){
+
+  if(source == "gallery"){
+    console.log("galleria")
+    const options: CameraOptions = {
+  quality: 100,
+  sourceType: this.camera.PictureSourceType.SAVEDPHOTOALBUM,
+  destinationType: this.camera.DestinationType.DATA_URL,
+
+  mediaType: this.camera.MediaType.PICTURE}
+
+  this.camera.getPicture(options).then((imageData) => {
+   // imageData is either a base64 encoded string or a file URI
+   // If it's base64:
+   this.base64Image = 'data:image/jpeg;base64,' + imageData;
+
+   console.log(""+this.base64Image)
+  }, (err) => {
+   // Handle error
+  });
+
+
+  }
+  else{
+    console.log("foto")
+    const options: CameraOptions = {
+  quality: 100,
+  sourceType: this.camera.PictureSourceType.CAMERA ,
+  destinationType: this.camera.DestinationType.DATA_URL,
+}
+
+this.camera.getPicture(options).then((imageData) => {
+ // imageData is either a base64 encoded string or a file URI
+ // If it's base64:
+ this.base64Image = 'data:image/jpeg;base64,' + imageData;
+
+ console.log(""+this.base64Image)
+}, (err) => {
+ // Handle error
+});
+
+  }
+
+
+}
+
 }
