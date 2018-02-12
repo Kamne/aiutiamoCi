21ccd6dd5f7848fa72d38c720b27584e7ea2c349
Nicola De Luca <nico.deluca@live.it>
2018-02-12 10:22:31 +0100

    Modificata leggermente l'interfaccia grafica della pagina Rubrica e aggiunte le funzionalità relativa ad essa
    Modificata leggermente l'interfaccia grafica della pagina Rubrica e
    aggiunte le funzionalità provvisorie  relative ad essa. Tale
    funzionalità ci permette di visualizzare gli assistenti e le
    associazioni. I nomi degli assistenti e delle associazioni vengono
    presi dal nostro database.

--- src/pages/rubrica/rubrica.ts
+++ src/pages/rubrica/rubrica.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Http, Headers } from "@angular/http";
 
 /**
  * Generated class for the RubricaPage page.
@@ -14,12 +15,54 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'rubrica.html',
 })
 export class RubricaPage {
+  public data:any = {};
+  searchQuery: string = '';
+  public list:any = {};
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http,) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RubricaPage');
   }
 
+  getAssistenti() {
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded' );
+    //let options = new RequestOptions({ headers: headers });
+    this.http.get('http://aiutiamoc.altervista.org/getAssistenti.php')
+  .subscribe(data => {
+    this.data.response=data["_body"].split(";");
+    this.data.response.pop();
+    }, error => {
+      alert("Oooops!");
+    });
+  }
+
+  getAssociazioni(){
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded' );
+    this.http.get('http://aiutiamoc.altervista.org/getAssociazioni.php')
+  .subscribe(data => {
+      console.log(data);
+      this.data.response=data["_body"].split(";");
+      this.data.response.pop();
+    }, error => {
+      alert("Oooops!");
+    });
+  }
+
+  generateList() {
+    this.list=this.data.response;
+  }
+
+  getItems(ev: any) { 
+    this.generateList();
+    let serVal = ev.target.value;
+    if (serVal && serVal.trim() != '') {
+      this.data.response = this.data.response.filter((topic) => {
+        return (topic.toLowerCase().indexOf(serVal.toLowerCase()) > -1);
+      })
+    }
+}
 }
