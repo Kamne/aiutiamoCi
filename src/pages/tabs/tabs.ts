44d4f5ea938c9ea9523b057a5fa6d20a2d8256db
Vincenzo Nastro <vinctusor93@gmail.com>
2018-02-13 09:36:06 +0100

    aggiornamento tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,19 +1,30 @@
 import { Component } from '@angular/core';
-
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { ListaCompetenzePage } from '../lista-competenze/lista-competenze';
+import { CompetenzeSelectedPage } from '../competenze-selected/competenze-selected';
+
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
+  //@ViewChild('myTabs') tabRef: Tabs;
+  params:any;
+  tab1Root = ListaCompetenzePage;
+  tab2Root = CompetenzeSelectedPage;
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  constructor(public navParams: NavParams) {
+  //  this.params = params;
+ // returns NavParams {data: Object}
 
-  constructor() {
 
+ // this tells the tabs component which Pages should be each tab's root Page
+
+  }
+  aiuto(){
+    this.params = { id: 42 };
   }
 }
