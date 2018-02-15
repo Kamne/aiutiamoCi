2186d109b1c76a494279681c95d06562af922d7d
Nicola De Luca <nico.deluca@live.it>
2018-02-15 14:33:24 +0100

    Aggiunta la componente grafica FAQ (solo per gli amministratori) e le relative funzionalità
    Aggiunta la componente grafica FAQ (solo per gli amministratori) e le
    relative funzionalità. L’admin, oltre a visualizzare le domande con le
    risposte effettuate dallo stesso, deve rispondere delle determinate
    domande poste dagli utenti. Manca, però, al momento la funzionalità che
    permette all’admin di rispondere una determinata domanda.

--- src/pages/faq-admin/faq-admin.ts
+++ src/pages/faq-admin/faq-admin.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Http, Headers, RequestOptions } from "@angular/http";
+import 'rxjs/add/operator/map';
+import { Dialogs } from '@ionic-native/dialogs';
+import { ClickBlock } from 'ionic-angular/components/app/click-block';
 
 /**
  * Generated class for the FaqAdminPage page.
@@ -15,11 +20,74 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FaqAdminPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public data: any = {};
+  public domande:any = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, public dialogs: Dialogs, public formBuilder: FormBuilder) {
+    //this.faqList();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FaqAdminPage');
   }
 
+  faqList() {
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded' );
+    let options = new RequestOptions({ headers: headers });
+    this.http.post('http://aiutiamoc.altervista.org/getDomande.php',options).map(res => res.json()).subscribe(    data => {
+      //console.log("myjson",JSON.parse(data.nonRisposte[1]));
+
+      var node = document.getElementById("domanda");
+      var elements = data.Risposte.length;
+
+      for (var i=0; i<elements; i++) {
+        console.log("myjson"+i,JSON.parse(data.Risposte[i]));
+        var parse = JSON.parse(data.Risposte[i]);
+        var h1 = document.createElement("H1");
+        var domText = document.createTextNode(parse.domanda);
+        h1.appendChild(domText);
+        node.appendChild(h1);
+        var h3 = document.createElement("H3");
+        var domRisp = document.createTextNode(parse.risposta);
+        h3.appendChild(domRisp);
+        node.appendChild(h3);
+        var br = document.createElement("BR");
+        node.appendChild(br);
+      }
+      // iterazione di JSON.parse(data.Risposte[i]) 
+      // andare nella console del browser (ISPEZIONA ELEMENTO)
+      
+      
+    });
+  } 
+
+  rispondiDomande() { 
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded' );
+    let options = new RequestOptions({ headers: headers });
+    this.http.post('http://aiutiamoc.altervista.org/getDomande.php',options).map(res => res.json()).subscribe(    data => {
+      var node = document.getElementById("rispondi");      
+      var numElem = data.nonRisposte.length;
+      alert(numElem);
+      for(var i = 0 ;i<numElem;i++) {
+
+        console.log("myjson"+i,JSON.parse(data.nonRisposte[i]));
+        var parse = JSON.parse(data.nonRisposte[i]);
+        var h1 = document.createElement("H1");
+        var domText = document.createTextNode(parse.domanda);
+        h1.appendChild(domText);
+        node.appendChild(h1);
+       
+        // creare il bottone e la relativa funzionalità
+        var btn = document.createElement("BUTTON");
+        btn.setAttribute("value",parse.domanda);
+        var t = document.createTextNode("RISPONDI");
+        btn.appendChild(t);
+        node.appendChild(btn);
+        var br = document.createElement("BR");
+        node.appendChild(br);
+       }
+    });
+  }
 }
