26f8a7b4c9f2a28fffdaf37d5ad7fc3ffeb8c6b6
Vincenzo Nastro <vinctusor93@gmail.com>
2018-02-17 12:23:19 +0100

    creazione tabs ricerca e relstive Pages

---
+++ src/pages/ricerca-richieste/ricerca.ts
@@ -0,0 +1,137 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
+import { Http, Headers, RequestOptions } from "@angular/http";
+import {
+ LatLng,
+ Spherical
+} from '@ionic-native/google-maps';
+import { Geolocation } from '@ionic-native/geolocation';
+import { ShareService } from '../../providers/shareService';
+import { NativeStorage } from '@ionic-native/native-storage';
+import { AlertController } from 'ionic-angular';
+import { NativeGeocoder, NativeGeocoderReverseResult, NativeGeocoderForwardResult } from '@ionic-native/native-geocoder';
+import { TabsPage } from '../tabs/tabs';
+
+@IonicPage()
+@Component({
+  selector: 'page-ricerca',
+  templateUrl: 'ricerca.html',
+})
+export class RicercaRichiestePage {
+indirizzi:Array<any>=[];
+searchCompetenze:Array<string>=[];
+allCompetenze:Array<any>=[];
+result:Array<string>=[];
+firstValue:number;
+secondValue:number;
+my: LatLng;
+other: LatLng;
+index:number;
+distance:number;
+checkAutomunito:boolean=false;
+checkPatentato:boolean=false;
+checkUrgenze:boolean=false;
+
+
+  constructor(public modalCtrl: ModalController,public alertCtrl: AlertController,public nativeStorage: NativeStorage, geolocation: Geolocation, public nativeGeocoder: NativeGeocoder,public shareService: ShareService,public http:Http,public spherical: Spherical,public navCtrl: NavController, public navParams: NavParams) {
+
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad RicercaPage');
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded' );
+    let options = new RequestOptions({ headers: headers });
+    this.http.post('http://aiutiamoc.altervista.org/ricercaUtenti.php',options).map(res => res.json()).subscribe(   data => {
+      console.log("data",data)
+      this.allCompetenze = data.competenze;
+      this.indirizzi = data.indirizzi;
+      this.nativeGeocoder.forwardGeocode(this.shareService.getUser().getIndirizzo()+","+this.shareService.getUser().getCitta())
+        .then((coordinates: NativeGeocoderForwardResult) =>{
+          var lat = Number(coordinates.latitude);
+          var lng = Number(coordinates.longitude);
+
+          this.my = new LatLng(lat,lng);
+
+        })
+        .catch((error: any) => console.log(error))
+   })
+
+  }
+
+
+ricerca(patentato: HTMLInputElement, urgenza: HTMLInputElement,  automunito: HTMLInputElement){
+console.log("automunito",automunito.value,this.checkAutomunito)
+
+this.result=[];
+this.index = 0;
+for( this.index = 0; this.index <this.indirizzi.length;this.index++){
+      this.aiutatm(this.index,patentato.value,urgenza.value,automunito.value);
+}
+this.vaccini(this.result);
+
+}
+
+
+aiutatm(index,patentato,urgenza,automunito){
+  this.nativeGeocoder.forwardGeocode(JSON.parse(this.indirizzi[this.index]).Indirizzo+","+JSON.parse(this.indirizzi[this.index]).Citta)
+    .then((coordinates: NativeGeocoderForwardResult) =>{
+      var lat = Number(coordinates.latitude);
+      var lng = Number(coordinates.longitude);
+      console.log("lat",lat,"lng",lng)
+      this.other = new LatLng(lat,lng);
+      this.distance = Spherical.computeDistanceBetween(this.my,this.other)
+      if((this.distance/1000) <= this.firstValue){
+        this.result.push(JSON.parse(this.indirizzi[index]).Username)
+        console.log("username",JSON.parse(this.indirizzi[index]).Username,this.distance/1000)
+    }
+    var myData = JSON.stringify({tipologia:"Richiesta",risultati:this.result,competenze:this.searchCompetenze.toString(),patentato:Number(this.checkPatentato),urgenza:Number(this.checkUrgenze),automunito:Number(this.checkAutomunito)});
+
+    if(index == (this.indirizzi.length-1)){
+      var headers = new Headers();
+      headers.append('Content-Type', 'application/x-www-form-urlencoded' );
+      let options = new RequestOptions({ headers: headers });
+
+      console.log("myData",myData);
+      this.http.post('http://aiutiamoc.altervista.org/risultatiRicercaUtenti.php',myData,options).map(res => res.json()).subscribe(   data => {
+      console.log("post",data);
+
+    })
+    }
+
+
+    })
+    .catch((error: any) => console.log(error));
+}
+
+
+vaccini(res){
+   console.log("vaccini",this.result.length);
+   console.log("vaccini",this.result);
+  var headers = new Headers();
+  headers.append('Content-Type', 'application/x-www-form-urlencoded' );
+  let options = new RequestOptions({ headers: headers });
+
+}
+
+
+
+openModal() {
+
+  this.shareService.setMyCompetenze(this.searchCompetenze)
+  this.shareService.setOtherCompetenze(this.allCompetenze)
+  let obj = {page: false};
+  let myModal = this.modalCtrl.create(TabsPage,obj);
+  myModal.present();
+
+  myModal.onDidDismiss(data => {
+
+      this.searchCompetenze = this.shareService.getMyCompetenze();
+      this.allCompetenze = this.shareService.getOtherCompetenze()
+
+
+});
+
+}
+
+}
