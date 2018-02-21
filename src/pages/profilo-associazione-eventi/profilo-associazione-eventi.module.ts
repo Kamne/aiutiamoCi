b87d259963005e3b8045f8c165a62eb28635e76b
Nicola De Luca <nico.deluca@live.it>
2018-02-21 11:35:10 +0100

    Creata la pagina profilo-associazione-eventi.

---
+++ src/pages/profilo-associazione-eventi/profilo-associazione-eventi.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ProfiloAssociazioneEventiPage } from './profilo-associazione-eventi';
+
+@NgModule({
+  declarations: [
+    ProfiloAssociazioneEventiPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ProfiloAssociazioneEventiPage),
+  ],
+})
+export class ProfiloAssociazioneEventiPageModule {}
