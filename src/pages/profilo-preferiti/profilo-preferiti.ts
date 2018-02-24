030d972d058c98cfc465b7864542125f5325d90e
Vincenzo Nastro <vinctusor93@gmail.com>
2018-02-24 18:39:35 +0100

    aggiornamento profilo-preferiti

--- src/pages/profilo-preferiti/profilo-preferiti.ts
+++ src/pages/profilo-preferiti/profilo-preferiti.ts
@@ -6,6 +6,9 @@ import { ShareService } from '../../providers/shareService';
 import { Associazione } from '../../classes/associazione';
 import { Utente } from '../../classes/utente';
 import { TabsProfiloUtentePage } from '../tabs-profilo-utente/tabs-profilo-utente';
+import { Dialogs } from '@ionic-native/dialogs';
+import { AlertController } from 'ionic-angular';
+import { Events } from 'ionic-angular';
 /**
  * Generated class for the ProfiloPreferitiPage page.
  *
@@ -19,12 +22,12 @@ import { TabsProfiloUtentePage } from '../tabs-profilo-utente/tabs-profilo-utent
   templateUrl: 'profilo-preferiti.html',
 })
 export class ProfiloPreferitiPage {
+nuovo:string='';
+preferiti:Array<string> =[];
 
-preferiti:Array<string>
+  constructor(public events: Events,public alertCtrl: AlertController,public dialogs: Dialogs,public shareService: ShareService, public navCtrl: NavController, public navParams: NavParams,public http:Http) {
 
-  constructor(public shareService: ShareService, public navCtrl: NavController, public navParams: NavParams,public http:Http) {
-
-    var headers = new Headers();
+/*    var headers = new Headers();
     headers.append('Content-Type', 'application/x-www-form-urlencoded' );
     let options = new RequestOptions({ headers: headers });
     var myData = JSON.stringify({
@@ -35,7 +38,11 @@ preferiti:Array<string>
       this.preferiti = data.preferiti
       console.log("Preferiti",data);
 
-   })
+   })*/
+   if(navParams.get('other') != undefined)
+   this.preferiti = this.shareService.getOtherUser().getPreferiti()
+   else
+   this.preferiti = this.shareService.getUser().getPreferiti()
   }
 
   ionViewDidLoad() {
@@ -47,7 +54,7 @@ getInfo(username){
   headers.append('Content-Type', 'application/x-www-form-urlencoded' );
   let options = new RequestOptions({ headers: headers });
   var myData = JSON.stringify({
-    username: this.shareService.getUser().getUsername()
+    username: username
   });
   this.http.post('http://aiutiamoc.altervista.org/getUtente.php',myData,options).map(res => res.json()).subscribe(   data => {
     console.log(data);
@@ -68,13 +75,65 @@ getInfo(username){
       this.shareService.setOtherUser(new Utente(data.user.Nome,data.user.Cognome,data.user.Username,data.user.Immagine,
                                            data.user.Nato,competenze,data.user.TitoloStudio,data.user.CF,
                                            data.user.Citta,data.user.Provincia,data.user.Indirizzo,
-                                           data.user.Email,data.user.NumTelefono,data.user.Tipologia,data.user.Sesso))
+                                           data.user.Email,data.user.NumTelefono,data.user.Tipologia,data.user.Sesso,data.preferiti))
       console.log(this.shareService.getOtherUser())
 
     }
-    let obj = {other: true};
-   this.navCtrl.push(TabsProfiloUtentePage,obj);
+    console.log("getOtherUser",this.shareService.getOtherUser());
+    this.profilo();
+
 } //if
 })
 }//getInfo
+
+profilo(){
+  let obj = {other: true,miracolo:true};
+  this.events.publish('other', obj);
+}
+
+alert_preferiti(){
+  let alert = this.alertCtrl.create({
+    title: 'Preferiti',
+    message:'Digitare l\' username dell\'utente da inserire',
+    cssClass:"alert",
+    inputs: [
+      {
+        name: 'preferito',
+        type: 'text',
+        value:this.nuovo,
+
+      }
+    ],
+
+    buttons: ['Annulla' ,
+    {
+      text: 'Conferma',
+      handler: data => {
+        console.log(data)
+        this.insert_preferito(data.preferito)
+      }
+    }]
+  });
+  alert.present();
+}
+
+insert_preferito(nome){
+
+  var headers = new Headers();
+  headers.append('Content-Type', 'application/x-www-form-urlencoded' );
+  let options = new RequestOptions({ headers: headers });
+  if(this.navParams.get('other') != undefined)
+  var myData = JSON.stringify({utente: this.shareService.getOtherUser().getUsername(),preferito: nome});
+  else
+  var myData = JSON.stringify({utente: this.shareService.getUser().getUsername(),preferito: nome});
+  console.log("campania",myData)
+  this.http.post('http://aiutiamoc.altervista.org/Insert_Preferito.php',myData,options).map(res => res.json()).subscribe(   data => {
+    console.log("ciccio",data);
+    this.dialogs.alert(""+data.message)
+    if(data.message == 'Operazione eseguita con successo')
+      this.preferiti.push(nome)
+  })
+
+}
+
 }
